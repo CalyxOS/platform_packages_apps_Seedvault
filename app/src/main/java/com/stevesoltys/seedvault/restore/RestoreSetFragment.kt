b491e52d05e91d66f261698279f9f9a505be040d
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-08-27 21:50:04 +0000

    RestoreSet: Pass on RESULT_SKIP
    * Grabbed from commit "Seedvault: redesign SUW activities"
      I2effdf5f11c8d92899f328abf0027e7bfd1a57ba
    
    Change-Id: Ie81407ee79c16d6c40a352efa846a05eac1ada37

--- app/src/main/java/com/stevesoltys/seedvault/restore/RestoreSetFragment.kt
+++ app/src/main/java/com/stevesoltys/seedvault/restore/RestoreSetFragment.kt
@@ -1,5 +1,6 @@
 package com.stevesoltys.seedvault.restore
 
+import android.app.Activity.RESULT_FIRST_USER
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -21,7 +22,7 @@ class RestoreSetFragment : Fragment() {
     private lateinit var listView: RecyclerView
     private lateinit var progressBar: ProgressBar
     private lateinit var errorView: TextView
-    private lateinit var skipView: TextView
+    private lateinit var skipView: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,6 +51,10 @@ class RestoreSetFragment : Fragment() {
 
         skipView.setOnClickListener {
             viewModel.onFinishClickedAfterRestoringAppData()
+            // Equivalent to com.google.android.setupcompat.util.ResultCodes.RESULT_SKIP
+            // SetupWizard handles this
+            requireActivity().setResult(RESULT_FIRST_USER)
+            requireActivity().finishAfterTransition()
         }
     }
 
